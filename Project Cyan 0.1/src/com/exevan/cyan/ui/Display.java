11a2fee67ef045bb055512e294517c6c82c179b4
Exevan <exevan110@gmail.com>
2014-09-18 00:58:34 +0200

    Added dynamic grid

--- Project Cyan 0.1/src/com/exevan/cyan/ui/Display.java
+++ Project Cyan 0.1/src/com/exevan/cyan/ui/Display.java
@@ -2,6 +2,8 @@ package com.exevan.cyan.ui;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 
@@ -12,14 +14,24 @@ public class Display extends JPanel {
 	private Renderer renderer;
 	
 	public Display() {
-		this.renderer = new Renderer();
+		this.renderer = new Renderer(this.getWidth(), this.getHeight());
 		this.setBackground(Color.BLACK);
+		MouseAdapter mouseAdapter = new MouseAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				super.mouseDragged(e);
+				renderer.updateSreenPosition(e.getX(), e.getY());
+				repaint();
+			}
+		};
+		this.addMouseListener(mouseAdapter);
+		this.addMouseMotionListener(mouseAdapter);
 	}
 	
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
-		renderer.drawGrid(this.getWidth(), this.getHeight(), g);
+		renderer.drawGrid(g);
 	}
 
 }
--- Project Cyan 0.1/src/com/exevan/cyan/ui/Renderer.java
+++ Project Cyan 0.1/src/com/exevan/cyan/ui/Renderer.java
@@ -1,15 +1,35 @@
 package com.exevan.cyan.ui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Toolkit;
 
 public class Renderer {
 	
-	public void drawGrid(int width, int height, Graphics g) {
+	int x, y;
+	int width, height;
+	
+	public Renderer(int width, int height) {
+		this.x = 0;
+		this.y = 0;
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		this.width = screen.width;
+		this.height = screen.height;
+	}
+	
+	public void updateSreenPosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void drawGrid(Graphics g) {
+		int dx = (x % CELL_SIZE) - CELL_SIZE;
+		int dy = (y % CELL_SIZE) - CELL_SIZE;
 		g.setColor(Color.GREEN);
 		for(int i = 0; i <= width; i += CELL_SIZE) {
 			for(int j = 0; j <= height; j += CELL_SIZE) {
-				g.drawRect(i, j, width, height);
+				g.drawRect(i + dx, j + dy, width, height);
 			}
 		}
 	}
